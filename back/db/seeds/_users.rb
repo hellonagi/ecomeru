88dab33fee4677f012e17a69f15afdc53cca43e1
hellonagi <38994662+hellonagi@users.noreply.github.com>
2024-09-28 00:20:17 +0900

    Add seed data

---
+++ back/db/seeds/_users.rb
@@ -0,0 +1,29 @@
+User.create(
+  provider: 'discord',
+  uid: '111111111111111111',
+  name: 'user1',
+  username: 'username1',
+  nickname: 'ユーザー1',
+  username_created_at: '2024-09-15 18:26:00.425846',
+  username_updated_at: '2024-09-15 18:26:00.425846'
+)
+
+User.create(
+  provider: 'discord',
+  uid: '111111111111111112',
+  name: 'user2',
+  username: 'username2',
+  nickname: 'ユーザー2',
+  username_created_at: '2024-09-15 18:26:00.425846',
+  username_updated_at: '2024-09-15 18:26:00.425846'
+)
+
+User.create(
+  provider: 'discord',
+  uid: '111111111111111113',
+  name: 'user3',
+  username: 'username3',
+  nickname: 'ユーザー3',
+  username_created_at: '2024-09-15 18:26:00.425846',
+  username_updated_at: '2024-09-15 18:26:00.425846'
+)
